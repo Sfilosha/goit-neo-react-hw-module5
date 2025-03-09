6e78d52e1529163d98eb94a6ceed5aebd65932f1
Sfilosha <sfilosha@gmail.com>
2025-03-09 14:44:28 +0200

    fix moviepage useEffect

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -7,7 +7,6 @@ import Loader from "../components/Loader/Loader.jsx";
 import MoviesGallery from "../components/MoviesGallery/MoviesGallery.jsx";
 
 export function MoviesPage() {
-  const [queryValue, setQueryValue] = useState("");
   const [moviesList, setMoviesList] = useState([]);
   const [nothingFound, setNothingFound] = useState(false);
   const [loader, setLoader] = useState(false);
@@ -19,43 +18,36 @@ export function MoviesPage() {
   // Search Params
   const [searchParams, setSearchParams] = useSearchParams();
   const search = searchParams.get("search");
-  // console.log("searchParams: ", search);
 
   const handleSearch = (query) => {
-    setQueryValue(query);
     setMoviesList([]);
     setSearchParams({ search: query });
   };
 
   useEffect(() => {
     if (!search) return;
-    setQueryValue(search);
-  }, [search]);
 
-  const fetching = async (query) => {
-    if (!query) return;
-    try {
-      setError(false);
-      setLoader(true);
-      setNothingFound(false);
+    const fetching = async () => {
+      try {
+        setError(false);
+        setLoader(true);
+        setNothingFound(false);
 
-      const fetchResult = await getByQuery(query); // Використовуй query
-      setMoviesList((prevImg) => [...prevImg, ...fetchResult.results]);
+        const fetchResult = await getByQuery(search);
+        setMoviesList((prevImg) => [...prevImg, ...fetchResult.results]);
 
-      if (fetchResult.results.length === 0) {
-        setNothingFound(true);
+        if (fetchResult.results.length === 0) {
+          setNothingFound(true);
+        }
+      } catch (error) {
+        console.error(error);
+        setError(true);
+      } finally {
+        setLoader(false);
       }
-    } catch (error) {
-      console.error(error);
-      setError(true);
-    } finally {
-      setLoader(false);
-    }
-  };
-
-  useEffect(() => {
-    fetching(queryValue); //
-  }, [queryValue]);
+    };
+    fetching();
+  }, [search]);
 
   return (
     <main>
